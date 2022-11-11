70b6061420d5e3732c0ca8396bd731a8551f2af1
Jonathan Zisser <jzisser9@gmail.com>
2022-11-10 22:22:48 -0500

    Added mul-api hostname

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module MulApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << "mul-api.duckdns.org"
   end
 end
