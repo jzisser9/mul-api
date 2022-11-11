3be3ea3d6333fd70546af6fcf662d46a40c80378
Jonathan Zisser <jzisser9@gmail.com>
2022-11-10 23:26:12 -0500

    Removed unofficial units

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@ Dir.entries(Rails.root.join('data')).select { |entry| %w[. .. .keep].exclude?(en
   Dir.glob("#{Rails.root}/data/#{type}/**/*.mtf") do |file|
     putsd "Processing file #{file}"
     lines = File.readlines(file)
+    mul_id = lines[3].scan(/\d+/).first.to_i
+    next if mul_id.zero?
+
     name = lines[1].gsub("\r\n", '')
     variant = lines[2].gsub("\r\n", '')
-    mul_id = lines[3].scan(/\d+/).first.to_i
-
     Unit.where(name: name, variant: variant, unit_type: unit_type, mul_id: mul_id).first_or_create
     putsd "Processed #{name} #{variant}"
   end
@@ -20,10 +21,12 @@ Dir.entries(Rails.root.join('data')).select { |entry| %w[. .. .keep].exclude?(en
   Dir.glob("#{Rails.root}/data/#{type}/**/*.blk") do |file|
     putsd "Processing file #{file}"
     blk_file = BlkFile.new(file)
+    mul_id = blk_file.value_in('mul id:').to_i
+    next if mul_id.zero?
+
     name = blk_file.value_in('Name')
     raise "name is blank!" if name.nil?
     variant = blk_file.value_in('Model')
-    mul_id = blk_file.value_in('mul id:').to_i
     raise "mul id is blank!" if mul_id.nil?
 
     Unit.where(name: name, variant: variant, unit_type: unit_type, mul_id: mul_id).first_or_create
