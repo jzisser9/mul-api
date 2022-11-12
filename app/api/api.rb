392551aee7b16099a60b9f50ebfc416d03dd4a18
Jonathan Zisser <jonathan@liftforward.com>
2022-11-11 22:03:27 -0500

    The default path returns something now

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,4 +3,29 @@ class API < Grape::API
 
   mount Units::API
   mount UnitTypes::API
+
+  get do
+    {
+      endpoints: [
+        {
+          path: '/units',
+          desc: 'Returns all units.',
+          filters: %w[name variant unit_type_id mul_id]
+        },
+        {
+          path: '/units/:id',
+          desc: 'Returns unit with the given :id.'
+        },
+        {
+          path: '/unit_types',
+          desc: 'Returns all unit types.',
+          filters: %w[name]
+        },
+        {
+          path: '/unit_types/:id',
+          desc: 'Returns unit type with the given :id.'
+        }
+      ]
+    }
+  end
 end
