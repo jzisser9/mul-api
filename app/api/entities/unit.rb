5f53d6cfc95aaff5ba4fa86e302ff1d1da13edf6
Jonathan Zisser <jzisser9@gmail.com>
2022-11-11 21:28:01 -0500

    Added id to unit

--- app/api/entities/unit.rb
+++ app/api/entities/unit.rb
@@ -1,5 +1,5 @@
 class Entities::Unit < Grape::Entity
-  expose :name, :variant, :unit_type_id, :mul_link
+  expose :id, :name, :variant, :unit_type_id, :mul_link
 
   with_options(if: { include_image_url: true }) do
     expose :image_url
