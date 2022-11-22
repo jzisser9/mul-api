b3a19e3285c8521b13162a1abf06cb43ab566bcd
Jonathan Zisser <jzisser9@gmail.com>
2022-11-22 17:31:05 -0500

    Added ubuntu server for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins 'http://localhost:3000'
+    origins 'http://192.168.86.2:3000'
 
     resource "*",
       headers: :any,
